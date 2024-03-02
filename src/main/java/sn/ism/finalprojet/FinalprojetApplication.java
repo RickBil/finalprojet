45b60eea34930e424805bf4dbbca07872ccfe7bc
rickchristian <rickchristian241@gmail.com>
2024-03-02 16:47:14 +0000

    Final_Projet

---
+++ src/main/java/sn/ism/finalprojet/FinalprojetApplication.java
@@ -0,0 +1,13 @@
+package sn.ism.finalprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/ism/finalprojet/FinalprojetApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.ism.finalprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
